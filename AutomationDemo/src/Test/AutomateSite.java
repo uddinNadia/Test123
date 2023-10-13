36f37f876237b6b06af0cacee95bfee9a55922a7
DELL <DELL@DESKTOP-F403C98.fios-router.home>
2023-10-12 20:27:50 -0400

    My first commit.

---
+++ AutomationDemo/src/Test/AutomateSite.java
@@ -0,0 +1,15 @@
+package Test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AutomateSite {
+
+	public static void main(String[] args) {
+	
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Downloads\\chromedriver-win64\\chromedriver-win64");
+		ChromeDriver driver = new ChromeDriver();
+		
+
+	}
+
+}
